c15a4293287e8698eaf9a8344a2e86e115613f2f
Sevinj Shahmaliyeva <shahmaliyevasevinj@gmail.com>
2025-07-18 21:11:36 +0400

    adding new Who is my pair game

---
+++ FarmEducation/View/WhoIsMyPairView.swift
@@ -0,0 +1,18 @@
+//
+//  WhoIsMyPairView.swift
+//  FarmEducation
+//
+//  Created by Sevinj Shahmaliyeva on 17.07.25.
+//
+
+import SwiftUI
+
+struct WhoIsMyPairView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhoIsMyPairView()
+}
---
+++ FarmEducation/View/WhoseTailIsThisView.swift
@@ -0,0 +1,18 @@
+//
+//  WhoseTailIsThisView.swift
+//  FarmEducation
+//
+//  Created by Sevinj Shahmaliyeva on 16.07.25.
+//
+
+import SwiftUI
+
+struct WhoseTailIsThisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhoseTailIsThisView()
+}
